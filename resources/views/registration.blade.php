e1134aea0fad11970735a4cc2f0254aab3167e72
Lucky <l.mendoza@bcrvtvi.edu.ph>
2023-01-24 12:51:40 -1200

    Update registration.blade.php

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -6,7 +6,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0" />
 
-  <title>Tooth Fairy: Dental Clinic </title>
+  <title>Tooth Fairy: Dental Clinic:></title>
 
   <meta name="description" content="" />
 
